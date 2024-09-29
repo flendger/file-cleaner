c89e74c103b92c9c97289b2ce7469772850b229a
e.kiru <evgeny.kiru@v6services.ru>
2024-09-29 23:13:53 +0300

    added dir_remover

--- src/dir_scanner.rs
+++ src/dir_scanner.rs
@@ -1,10 +1,8 @@
 use std::io::Error;
 use std::path::Path;
 
-pub fn scan_dirs(folder: &String) -> Result<Vec<Box<Path>>, Error> {
-    let path = Path::new(folder);
-
-    let dirs = path.read_dir()?
+pub fn scan_dirs(folder: &Path) -> Result<Vec<Box<Path>>, Error> {
+    let dirs = folder.read_dir()?
         .flat_map(|dir| dir)
         .map(|entry| entry.path())
         .filter(|path| path.is_dir())
