2c88707d7a1a49d7fed6d6c5c4ff790a2d64d3d5
e.kiru <evgeny.kiru@v6services.ru>
2024-09-29 23:14:28 +0300

    refactoring file_scanner

--- src/file_scanner.rs
+++ src/file_scanner.rs
@@ -3,11 +3,10 @@ use std::io::Error;
 use std::ops::Sub;
 use std::path::{Path, PathBuf};
 
-pub fn scan(folder: &String, depth: u64) -> Result<Vec<Box<Path>>, Error> {
+pub fn scan_files(folder: &Path, depth: u64) -> Result<Vec<Box<Path>>, Error> {
     let initial_date = resolve_date(depth);
 
-    let path = Path::new(&folder);
-    let files = path.read_dir()?
+    let files = folder.read_dir()?
         .flat_map(|dir| dir)
         .map(|dir| dir.path())
         .filter(|path| path.is_file())
