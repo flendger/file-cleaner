d974b418cb7744a58c0ecc753290c41807fa8b41
e.kiru <evgeny.kiru@v6services.ru>
2024-10-03 22:49:56 +0300

    added run_params

---
+++ src/run_params.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None, author)]
+pub struct Args {
+    #[arg(short = 'c', long, default_value = "")]
+    config: String,
+}
+
+pub fn get_args() -> Args {
+    Args::parse()
+}
