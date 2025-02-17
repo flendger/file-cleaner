521fd4d8f632fee9ef226bdb41968baee79ee0e2
e.kiru <evgeny.kiru@v6services.ru>
2025-02-17 09:12:10 +0300

    added Task trait

---
+++ src/tasks.rs
@@ -0,0 +1,8 @@
+use crate::settings::Settings;
+
+pub trait Task {
+    // TODO: 15.02.2025 ekiru --> use trait type ???
+
+    fn run(&self, settings: &Settings);
+    fn task_type(&self) -> char;
+}
