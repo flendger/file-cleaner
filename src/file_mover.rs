412ae2ce64a804b1e0161064e5e684afd6427a89
e.kiru <evgeny.kiru@v6services.ru>
2025-02-28 22:00:33 +0300

    refactoring

--- src/file_mover.rs
+++ src/file_mover.rs
@@ -3,9 +3,10 @@ use std::io::ErrorKind;
 use std::path::Path;
 use std::{fs, io};
 
-pub fn move_file(path: &Path, dest: &str) -> io::Result<()> {
+pub fn move_file(path: &Path, dest: &Path) -> io::Result<()> {
     if let Some(f_name) = path.file_name() {
-        let dest_path: String = dest.to_string() + "/" + f_name.to_str().unwrap();
+        let dest_path = dest.join(f_name);
+
         fs::rename(path, dest_path)
     } else {
         Err(Error::new(
